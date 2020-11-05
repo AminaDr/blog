1a116c13835fcc6f03a8722106abea5057e6d2b4
Amina <50828174+AminaDr@users.noreply.github.com>
2020-11-05 10:49:47 +0100

    Update routes.rb
    Löschen

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   get 'pages/about'
   get 'pages/contact'
-  resources:posts
+  resources :posts, except: [:destroy] do
+  get 'destroy', on: :member
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
